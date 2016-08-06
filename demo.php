d348ceb7e324019d574e910c9b2b1fd9c3fb541f
spiraspera <anton.kudashov@gmail.com>
2016-08-06 15:00:24 +0300

    final checks were added, some changes

--- demo.php
+++ demo.php
@@ -1,5 +1,8 @@
 <?
     include 'vd_timestamper.php';
-    var_dump(vd_timestamper('2016/12/22'));
-    // $input_type parameter is optional: dd-mm-yyyy, mm-dd-yyyy, yyyy-mm-dd
+    echo vd_timestamper($date='31/12/2016', $input_type='dd-mm-yyyy', $return_type='yyyy-mm-dd');
+    echo '<br>';
+    echo vd_timestamper($date='31/12/2016', $input_type='dd-mm-yyyy');
+    echo '<br>';
+    echo vd_timestamper($date='31/12/2016');
 ?>
--- vd_timestamper.php
+++ vd_timestamper.php
@@ -1,38 +1,23 @@
 <?
-// function array_counter($array) {
-//     $array_length = strlen($array);
-//     $temp_string = '';
-
-//     for ($i = 0; $i < $date_length; $i++) { 
-//         $char = $date[$i];
-
-//         if ($i+1 == $date_length) {
-//             $temp_string .= $char;
-//             array_push($char_list, $temp_string);
-//         } elseif (is_numeric($char)) {
-//             $temp_string .= $char;
-//         } else {    
-//             array_push($char_list, $temp_string);
-//             $temp_string = '';
-//         }
-
-//     }
-
-//     return $counted;
-// }
-
-
-function vd_timestamper($date, $input_type="", $return_type="timestamp") {
+function vd_timestamper($date, $input_type='', $return_type='timestamp') {
     // $input_type parameter is optional: dd-mm-yyyy, mm-dd-yyyy, yyyy-mm-dd
+    // $return_type parameter is optional: timestamp (default), dd-mm-yyyy, mm-dd-yyyy, yyyy-mm-dd
+
+    // possible function calls:
+    // vd_timestamper($date='31/12/2016', $input_type='dd-mm-yyyy', $return_type='yyyy-mm-dd');
+    // vd_timestamper($date='31/12/2016', $input_type='dd-mm-yyyy');
+    // vd_timestamper($date='31/12/2016');
 
     // exceptions
     $timestamper_error = 'Timestamper error: ';
     $not_a_string = $timestamper_error . 'date has to be a string!';
     $wrong_format = $timestamper_error . 'incorrect date format!';
     $wrong_input_type = $timestamper_error . 'incorrect input type format!';
+    $enter_input_type = $timestamper_error . 'please, enter input type format.';
 
     // parameters
-    $proper_types = array('dd-mm-yyyy' => '224', 'mm-dd-yyyy' => '224', 'yyyy-mm-dd' => '422');
+    $proper_input_types = array('dd-mm-yyyy' => '224', 'mm-dd-yyyy' => '224', 'yyyy-mm-dd' => '422');
+    $proper_return_types = array('timestamp', 'dd-mm-yyyy', 'mm-dd-yyyy', 'yyyy-mm-dd');
 
     // input check
     switch ($date) {
@@ -55,7 +40,7 @@ function vd_timestamper($date, $input_type="", $return_type="timestamp") {
         if ($i+1 == $date_length) {
             $temp_string .= $char;
             array_push($char_list, $temp_string);
-        } elseif (is_numeric($char)) {
+        } else if (is_numeric($char)) {
             $temp_string .= $char;
         } else {    
             array_push($char_list, $temp_string);
@@ -64,24 +49,26 @@ function vd_timestamper($date, $input_type="", $return_type="timestamp") {
 
     }
 
-    // check if array is correct
-    switch ($date) {
-        case count($char_list) !== 3:
-            return $wrong_format;   
-        default:
-            continue;
-    }    
+    // check if array has correct numbers of cells
+    if (count($char_list) !== 3) {
+        return $wrong_format;   
+    }      
 
-    // chech if type was entered
-    if (array_key_exists($input_type, $proper_types)) {
-        // create a string with numbers of chars in each char_list cells
-        foreach ($char_list as $key) {
-            $test_type .= strlen($key);
-        }
+    // create a string with numbers of chars in each char_list cell
+    foreach ($char_list as $key) {
+        // but firstly check if date range is correct
+        if (strlen($key) == 2 && $key > 31) {
+            return $wrong_format;
+        } else {
+            $numeric_type .= strlen($key);
+        }   
+    }
 
-        // check if numbers are equal to the proper_types value
-        if ($test_type != $proper_types[$input_type]) {
-            echo $wrong_input_type;
+    // check if input type was entered
+    if (array_key_exists($input_type, $proper_input_types)) {
+        // check if numbers are equal to the proper_input_types value
+        if ($numeric_type != $proper_input_types[$input_type]) {
+            return $wrong_input_type;
         } else {
             if ($input_type == 'dd-mm-yyyy') {
                 $day = $char_list[0];
@@ -97,25 +84,52 @@ function vd_timestamper($date, $input_type="", $return_type="timestamp") {
                 $year = $char_list[0];
             }
         }
-        
-
     } else {
-        // type wasnt entered
-        echo 'type is empty';
+        // type wasn't entered
+        switch ($numeric_type) {
+            case '224':
+                if ($char_list[0] > 12 && $char_list[1] <= 12) {
+                    // dd-mm-yyyy
+                    $day = $char_list[0];
+                    $month = $char_list[1];
+                    $year = $char_list[2];
+                    continue;
+                } else if ($char_list[0] <= 12 && $char_list[1] > 12) {
+                    // mm-dd-yyyy
+                    $day = $char_list[1];
+                    $month = $char_list[0];
+                    $year = $char_list[2];
+                    continue;
+                }  else {
+                    // can't recognize where is month or day
+                    // because $char_list[0] <= 12 && $char_list[1] <= 12
+                    return $enter_input_type;
+                }
+            case '422':    
+                if ($char_list[1] <= 12 && $char_list[2] > 12) {
+                    // yyyy-mm-dd
+                    $day = $char_list[2];
+                    $month = $char_list[1];
+                    $year = $char_list[0];
+                    continue;
+                }  else {
+                    // can't recognize where is month or day
+                    // because $char_list[1] <= 12 && $char_list[2] <= 12
+                    return $enter_input_type;
+                }            
+        }
     }
 
-    // return $day . $month . $year;
-    return $char_list;
-
-    // return array_keys($proper_types, '224');
-
-
-    // return array_search($test_type, $proper_types);
-
-
-    // $day = substr($date, 0, 2);
-    // $month = substr($date, 3, 2);
-    // $year = substr($date, 6, 4);
-    // return strtotime($month . '/' . $day . '/' . $year);
+    // check $return_type and return result
+    switch ($return_type) {
+        case $return_type == $proper_return_types[0]:
+            return strtotime($month . '/' . $day . '/' . $year);
+        case $return_type == $proper_return_types[1]:
+            return $day . '-' . $month . '-' . $year;
+        case $return_type == $proper_return_types[2]:
+            return $month . '-' . $day . '-' . $year;
+        case $return_type == $proper_return_types[3]:
+            return $year . '-' . $month . '-' . $day;
+    }
 }
 ?>
