9940f43db40a2f44354cbdbe330f59b1b273c26c
spiraspera <anton.kudashov@gmail.com>
2016-08-06 13:21:53 +0300

    check if input type was entered

--- demo.php
+++ demo.php
@@ -1,4 +1,5 @@
 <?
     include 'vd_timestamper.php';
-    var_dump(vd_timestamper('2016/05/05'));
+    var_dump(vd_timestamper('2016/12/22'));
+    // $input_type parameter is optional: dd-mm-yyyy, mm-dd-yyyy, yyyy-mm-dd
 ?>
--- vd_timestamper.php
+++ vd_timestamper.php
@@ -1,15 +1,38 @@
 <?
+// function array_counter($array) {
+//     $array_length = strlen($array);
+//     $temp_string = '';
+
+//     for ($i = 0; $i < $date_length; $i++) { 
+//         $char = $date[$i];
+
+//         if ($i+1 == $date_length) {
+//             $temp_string .= $char;
+//             array_push($char_list, $temp_string);
+//         } elseif (is_numeric($char)) {
+//             $temp_string .= $char;
+//         } else {    
+//             array_push($char_list, $temp_string);
+//             $temp_string = '';
+//         }
+
+//     }
+
+//     return $counted;
+// }
+
+
 function vd_timestamper($date, $input_type="", $return_type="timestamp") {
-    // $input_type parameter is optional: dd-mm-yyyy, mm-dd-yyyy, yyyy-mm-dd,
-    // separator doesn't matter
+    // $input_type parameter is optional: dd-mm-yyyy, mm-dd-yyyy, yyyy-mm-dd
 
     // exceptions
     $timestamper_error = 'Timestamper error: ';
     $not_a_string = $timestamper_error . 'date has to be a string!';
     $wrong_format = $timestamper_error . 'incorrect date format!';
+    $wrong_input_type = $timestamper_error . 'incorrect input type format!';
 
     // parameters
-    $proper_types = array('dd-mm-yyyy', 'mm-dd-yyyy', 'yyyy-mm-dd');
+    $proper_types = array('dd-mm-yyyy' => '224', 'mm-dd-yyyy' => '224', 'yyyy-mm-dd' => '422');
 
     // input check
     switch ($date) {
@@ -50,13 +73,44 @@ function vd_timestamper($date, $input_type="", $return_type="timestamp") {
     }    
 
     // chech if type was entered
-    if in_array($input_type, $proper_types) {
-        // check and compare 
+    if (array_key_exists($input_type, $proper_types)) {
+        // create a string with numbers of chars in each char_list cells
+        foreach ($char_list as $key) {
+            $test_type .= strlen($key);
+        }
+
+        // check if numbers are equal to the proper_types value
+        if ($test_type != $proper_types[$input_type]) {
+            echo $wrong_input_type;
+        } else {
+            if ($input_type == 'dd-mm-yyyy') {
+                $day = $char_list[0];
+                $month = $char_list[1];
+                $year = $char_list[2];
+            } else if ($input_type == 'mm-dd-yyyy') {
+                $day = $char_list[1];
+                $month = $char_list[0];
+                $year = $char_list[2];
+            } else {
+                $day = $char_list[2];
+                $month = $char_list[1];
+                $year = $char_list[0];
+            }
+        }
+        
+
+    } else {
+        // type wasnt entered
+        echo 'type is empty';
     }
 
-
-
+    // return $day . $month . $year;
     return $char_list;
+
+    // return array_keys($proper_types, '224');
+
+
+    // return array_search($test_type, $proper_types);
 
 
     // $day = substr($date, 0, 2);
